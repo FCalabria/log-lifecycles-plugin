b52278b64a1d52b76face261f8388d2c2d5254ba
Paqui <paqui.calabria@schibsted.com>
2019-01-23 14:59:47 +0100

    :gift: Base  plugin

---
+++ main.js
@@ -0,0 +1,21 @@
+export default {
+  install: (Vue) => {
+    const mixin = {}
+    const config = {
+      created: {msg: 'was created', color: '#04B431'},
+      mounted: {msg: 'was mounted', color: '#58ACFA'},
+      updated: {msg: 'was updated', color: '#DBA901'},
+      activated: {msg: 'was activated', color: '#0101DF'},
+      deactivated: {msg: 'was deactivated', color: '#B404AE'},
+      destroyed: {msg: 'was destroyed', color: '#B40404'}
+    }
+    const lifecycles = Object.keys(config)
+    function logLivecycle (componentName, lifecycle) {
+      const lifecycleConfig = config[lifecycle]
+      console.log(`%c ${componentName} ${lifecycleConfig.msg}`, `color:${lifecycleConfig.color}`)
+    }
+    lifecycles.forEach((name) => { mixin[name] = function () { logLivecycle(this.$options.name, name) } })
+    Vue.mixin(mixin)
+  }
+}
+
