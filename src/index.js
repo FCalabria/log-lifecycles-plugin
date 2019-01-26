aa0a1cbf0ae256942cd8e77c0ed4441f779ca8f1
Paqui Calabria <pcalabriarubio@gmail.com>
2019-01-26 18:31:23 +0000

    Add missing lifecycles
    Closes #2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ export default {
     if (process.env.NODE_ENV === 'production') return
     const mixin = {}
     const config = {
+      beforeCreate: {msg: 'is about to be created', color: '#04B431'},
       created: {msg: 'was created', color: '#04B431'},
+      beforeMount: {msg: 'is about to be mounted', color: '#58ACFA'},
       mounted: {msg: 'was mounted', color: '#58ACFA'},
+      beforeUpdate: {msg: 'is about to be updated', color: '#DBA901'},
       updated: {msg: 'was updated', color: '#DBA901'},
       activated: {msg: 'was activated', color: '#0101DF'},
       deactivated: {msg: 'was deactivated', color: '#B404AE'},
-      destroyed: {msg: 'was destroyed', color: '#B40404'}
+      beforeDestroy: {msg: 'is about to be destroyed', color: '#800000'},
+      destroyed: {msg: 'was destroyed', color: '#800000'},
+      errorCaptured: {msg: 'error', color: '#e60000'}
     }
     const lifecycles = Object.keys(config)
     function logLivecycle (componentName, lifecycle) {
